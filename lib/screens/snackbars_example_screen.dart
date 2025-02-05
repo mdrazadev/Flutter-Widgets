b522b0e3b863b0f8f327e598f969b1cbfb6b6f00
Mohd Raza Shaikh <mohdraza7058@gmail.com>
2025-02-05 14:16:33 +0530

    new snackbar added

--- lib/screens/snackbars_example_screen.dart
+++ lib/screens/snackbars_example_screen.dart
@@ -4,17 +4,26 @@ import 'package:flutter_widgets/widgets/buttons/simple_button_widget.dart';
 class SnackbarsExampleScreen extends StatelessWidget {
   const SnackbarsExampleScreen({super.key});
 
-  showSnackbar(BuildContext context, String label, {VoidCallback? onUndo}) {
+  showSnackbar(
+    BuildContext context,
+    String label, {
+    VoidCallback? onUndo,
+    bool? showCloseBtn = false,
+    Color? bgColor,
+  }) {
     ScaffoldMessenger.of(context).clearSnackBars();
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
         content: Text(label),
+        dismissDirection: DismissDirection.horizontal,
+        backgroundColor: bgColor,
         action: onUndo != null
             ? SnackBarAction(
                 label: 'undo',
                 onPressed: onUndo,
               )
             : null,
+        showCloseIcon: showCloseBtn,
       ),
     );
   }
@@ -50,18 +59,19 @@ class SnackbarsExampleScreen extends StatelessWidget {
               );
             },
           ),
-          // const SizedBox(
-          //   height: 15,
-          // ),
-          // SimpleButtonWidget(
-          //   label: 'Snackbar with close button',
-          //   onPress: () {
-          //     showSnackbar(
-          //       context,
-          //       "Snackbar with undo",
-          //     );
-          //   },
-          // ),
+          const SizedBox(
+            height: 15,
+          ),
+          SimpleButtonWidget(
+            label: 'Snackbar with close button',
+            onPress: () {
+              showSnackbar(
+                context,
+                "Snackbar with closing btn",
+                showCloseBtn: true,
+              );
+            },
+          ),
         ],
       ),
     );
