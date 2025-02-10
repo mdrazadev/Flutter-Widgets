2a52b7ab88ce279ce477636be1e57895f170b6f9
Mohd Raza Shaikh <mohdraza7058@gmail.com>
2025-02-10 13:05:25 +0530

    new snackbars added

--- lib/screens/snackbars_example_screen.dart
+++ lib/screens/snackbars_example_screen.dart
@@ -1,8 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_widgets/widgets/buttons/simple_button_widget.dart';
+import 'package:get/get.dart';
 
 class SnackbarsExampleScreen extends StatelessWidget {
   const SnackbarsExampleScreen({super.key});
+
+  /// This method is to show [uppper snackbar] to user.
+  ///
+  /// It accepts two arguments: [context] and [label].
+  ///
+  /// By default it's background color is [Grey]. You can pass any color you want
+  /// to [bgColor].
+  ///
+  /// By default it's foreground color is [Black]. You can pass any color you want
+  /// to [fgColor].
+  ///
+  /// If snackbar is for [warning], then you just need to pass [isWarning] as
+  /// [True].
+  static topSnackbar(
+    String title,
+    String message, {
+    Color? bgColor,
+    Color? fgColor,
+    bool isWarning = false,
+  }) {
+    Get.closeCurrentSnackbar();
+    Get.closeAllSnackbars();
+    Get.snackbar(
+      title,
+      message,
+      backgroundColor: isWarning ? Colors.red : bgColor,
+      colorText: isWarning ? Colors.white : fgColor,
+    );
+  }
 
   showSnackbar(
     BuildContext context,
@@ -69,6 +99,28 @@ class SnackbarsExampleScreen extends StatelessWidget {
                 context,
                 "Snackbar with closing btn",
                 showCloseBtn: true,
+              );
+            },
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          SimpleButtonWidget(
+            label: 'Simple upper snackbar',
+            onPress: () {
+              topSnackbar('Upper Snackbar', 'This is simple upper snackbar');
+            },
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          SimpleButtonWidget(
+            label: 'Warning upper snackbar',
+            onPress: () {
+              topSnackbar(
+                'Warning Snackbar',
+                'This is warning snackbar',
+                isWarning: true,
               );
             },
           ),
